8fffad3c902a648eee52cdd24ab74a774266d068
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2021-12-28 08:45:35 +0100

    update2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
 
     return (
-        <nav>
+        <nav className='relative'>
             <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
                 {Object.entries(requests).map(([key, { title, url }]) => (
                     <h2 
